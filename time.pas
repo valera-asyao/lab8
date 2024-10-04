e955cbc1b3665ba22ee3587eccd227d0e0742b05
Абрамов Валерий <your_valeraabramov763@gmail.com>
2024-10-04 13:07:48 +0300

    создал time.pas, изменил его для 3 задания

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var year := readinteger('Введите год');
+  if (year mod 4 = 0) and not((year mod 100 = 0) and (year mod 400 <> 0)) then
+    print('год високосный, 366 дней')
+  else
+    print('год обычныйб 365 дней');
+end.
